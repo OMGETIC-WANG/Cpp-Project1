1496be722c4d97be0bd6dada4b2ce833198d8c76
OMGETIC-WANG <omgetic@outlook.com>
2020-09-27 20:55:49 +0800

    commit1

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -12,10 +12,10 @@ HINSTANCE hInst;
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
 int CALLBACK WinMain(
-    _In_ HINSTANCE hInstance,
+    _In_ HINSTANCE     hInstance,
     _In_opt_ HINSTANCE hPrevInstance,
-    _In_ LPSTR     lpCmdLine,
-    _In_ int       nCmdShow
+    _In_ LPSTR         lpCmdLine,
+    _In_ int           nCmdShow
 )
 {
     WNDCLASSEX wcex;
